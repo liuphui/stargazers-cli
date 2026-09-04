bf2c06c001bb3297f4944d1507785d9db68dea25
Peng Hui Liu <liup1@tcd.ie>
2026-09-04 18:02:14 +0100

    Fixed second request not being authenticated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from typing import List
 import requests
-import json
 import os
 from dotenv import load_dotenv
 
@@ -14,7 +13,8 @@ def construct_headers() -> dict:
     
     headers = {
         "Authorization": f"Bearer {token}",
-        "Accept": "application/vnd.github+json"
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2026-03-10"
     }
     return headers
 
@@ -22,18 +22,17 @@ def fetch_repositories() -> List[dict]:
     get_public_repositories_url = "https://api.github.com/repositories"
     headers = construct_headers()
     response = requests.get(get_public_repositories_url, headers=headers).json()
-    print(response)
     return response
 
 def fetch_top_starred_by_language(given_language: str) -> dict:
     response = fetch_repositories()
+    headers = construct_headers()
     
     # { repo_name : { language1, language2 } }
     repo_by_language = {}
     for i in range(len(response)):
         repository_name = response[i].get("name", "")
-        language_response = requests.get(response[i].get("languages_url", "")).json()
-        print(language_response)
+        language_response = requests.get(response[i].get("languages_url", ""), headers=headers).json()
         if given_language in language_response:
             repo_by_language[repository_name] = language_response
     return repo_by_language
